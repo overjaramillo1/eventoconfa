d439a38f3d718c006650afd0f4004a95781ea07a
overjaramillo1 <jaramil161@gmail.com>
2023-03-13 18:54:37 -0500

    changes for v5

--- src/pages/Validacion.js
+++ src/pages/Validacion.js
@@ -5,102 +5,117 @@ export default function Validacion() {
   const [foto, setFoto] = useState();
   const webcamRef = React.useRef(null);
   const [imgTake, setimgTake] = React.useState(null);
-  const [data, setData] = useState('');
-  const [cc, setCc] = useState('');
+  const [data, setData] = useState("");
+  const [cc, setCc] = useState("");
   const canvasRef = useRef(null);
   /*CAPTURAR*************************** */
   const capture = React.useCallback(() => {
     // const ctx = canvasRef.current.getContext("2d");
-  //   drawRect('', ctx); 
- 
-     const imageSrc = webcamRef.current.getScreenshot();
-     var strImage = imageSrc.replace(/^data:image\/[a-z]+;base64,/, "");
-     setimgTake(imageSrc);
-     var requestOptions = {
-       method: "POST",
-       headers: {
-         "x-api-key": "da3j0LYdK46bGLzs67oCK1X23imFczh79e9XwbIy",
-       },
-       body: JSON.stringify({
-         imgdata: imageSrc.replace(/^data:image\/[a-z]+;base64,/, ""),
-       }),
-     };
- 
-     fetch(
-       "https://ssz3in99qf.execute-api.us-east-1.amazonaws.com/py/validacionfacialc2",
-       requestOptions
-     )
-       .then((response) => response.json())
-       .then((data) => {
-         console.log(data);
-         if(data.respuesta!=null){
-         setFoto(<label className="labelCC">*Documento encontrado: {data.respuesta}</label>);
-         setCc(data.respuesta);
-       }else{
-         setFoto(<label className="labelCC">**No se pudo reconer el rostro: {data.respuesta}</label>);
-       }
-       })
-       .catch((error) => console.log("error", error));
-       
-   });
- /*END CAPTURAR*************************** */
- 
- function limpiar(){
-   setFoto(null);
-   setimgTake(null);
-   setCc(null);
-   
- }
- 
-   return (
- <div>
- 
-   
-     <div >  <p className="a">Validación facial y registro ingreso</p></div>
- 
-     <div className="validacion">
- 
-       
-         <Webcam className="cam"
-           audio={false}
-           height={300}
-           ref={webcamRef}
-           screenshotFormat="image/jpeg"
-           width={480}
-         />  {imgTake && (
-           <img width={200} height={150}
-             src={imgTake}
-           />
-         )}
-        
-     </div>
- 
-     <canvas
-           ref={canvasRef}
-           screenshotFormat="image/jpeg"
-           style={{
-             position: "absolute",
-             marginLeft: "auto",
-             marginRight: "auto",
-             left: 0,
-             right: 0,
-             textAlign: "center",
-             zindex: 8,
-             width: 440,
-             height: 380,
-             border: 2,
-           }}
-           
-         />
+    //   drawRect('', ctx);
 
-     <button onClick={capture} className="btnFoto">Tomar foto</button>  
-     <button onClick={limpiar} className="btnFoto">Nuevo/limpiar</button>
+    const imageSrc = webcamRef.current.getScreenshot();
+    var strImage = imageSrc.replace(/^data:image\/[a-z]+;base64,/, "");
+    setimgTake(imageSrc);
+    var requestOptions = {
+      method: "POST",
+      headers: {
+        "x-api-key": "da3j0LYdK46bGLzs67oCK1X23imFczh79e9XwbIy",
+      },
+      body: JSON.stringify({
+        imgdata: imageSrc.replace(/^data:image\/[a-z]+;base64,/, ""),
+      }),
+    };
 
-     <div><h1>{foto} </h1></div>
- 
-         
- 
-     </div>
-   );
- }
- 
+    fetch(
+      "https://ssz3in99qf.execute-api.us-east-1.amazonaws.com/py/validacionfacialc2",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.respuesta != null) {
+          setFoto(
+            <label className="labelCC">
+              *Documento encontrado: {data.respuesta}--
+               Estado:{data.detalle}
+            </label>
+          );
+          setCc(data.respuesta);
+        } else {
+          setFoto(
+            <label className="labelCC">
+              **No se pudo reconer el rostro: {data.respuesta}
+            </label>
+          );
+        }
+      })
+      .catch((error) => console.log("error", error));
+  });
+  /*END CAPTURAR*************************** */
+
+  function limpiar() {
+    setFoto(null);
+    setimgTake(null);
+    setCc(null);
+  }
+
+  return (
+    <div>
+      <div>
+        {" "}
+        <p className="a">Validación facial y registro ingreso</p>
+      </div>
+      <br></br>
+      <div className="validacion">
+        <table className="center">
+          <thead>
+            <tr>
+              <th>
+                {" "}
+                <Webcam
+                  className="cam"
+                  audio={false}
+                  height={300}
+                  ref={webcamRef}
+                  screenshotFormat="image/jpeg"
+                  width={480}
+                />{" "}
+              </th>
+              <th>
+                {" "}
+                {imgTake && <img width={200} height={150} src={imgTake} />}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div className="outer">
+                  <div class="inner">
+                    <button onClick={capture} className="btnFoto">
+                      Tomar foto
+                    </button>
+                  </div>
+                </div>
+              </td>
+              <td>
+                <div className="outer">
+                  <div class="inner">
+                    <button onClick={limpiar} className="btnFoto">
+                      Nuevo/limpiar
+                    </button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h1>{foto} </h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
