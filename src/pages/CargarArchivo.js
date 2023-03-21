7585bde89e0c779b615a908e5523adf5310ae269
overjaramillo1 <jaramil161@gmail.com>
2023-03-21 15:06:22 -0500

    changes for v22

--- src/pages/CargarArchivo.js
+++ src/pages/CargarArchivo.js
@@ -13,143 +13,35 @@ export default function CargarArchivo() {
   var fileInput = null;
   const [file, setFile] = useState(null);
   const [filename, SetFilename] = useState("");
-  /*useEffect(() => {
-    var token = localStorage.getItem("tk");
-    var requestOptions = {
-      method: "GET",
-      headers: {
-        Authorization: token,
-      },
-    };
-    const getAsistentes = async () => {
-      const response = await fetch(
-        "https://xua8seev0d.execute-api.us-east-1.amazonaws.com/py/items",
-        requestOptions
-      );
-      const data = await response.json();
-      setEventos(Object.values(data.Items));
-      console.log("data :>> ", data);
-    };
-    getAsistentes();
-  }, []);
-*/
-
-  const enviarArchivo = async () => {
-    const formData = new FormData();
-    formData.append("File", file);
-    console.log("file ***:>> ", file);
-
-    try {
-      const res = await fetch(
-        "https://doq6msba36.execute-api.us-east-1.amazonaws.com/PY/upload",
-        {
-          method: "POST",
-          headers: {
-            filename: "prueba.pdf",
-            bucket: "confaanexospy/MPC/beneficios/postulaciones",
-            sistema: "MPC",
-          },
-          body: file,
-        }
-      );
-      const actualData = await res.json();
-      console.log(actualData);
-    } catch (error) {
-      console.log("error :>> ", error);
-    }
-  };
-
-  const handleCargarArchivo = async ({ target: { files } }) => {
-    try {
-      // const fileInput = document.querySelector('input[type="file"]');
-
-      const formData = new FormData();
-      formData.append("file", file);
-
-      fetch(
-        "https://doq6msba36.execute-api.us-east-1.amazonaws.com/PY/upload",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            filename: file,
-            bucket: "confaanexospy/MPC/beneficios/postulaciones",
-            sistema: "MPC",
-          },
-          body: formData,
-        }
-      )
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.error(error));
-    } catch (error) {
-      console.log("error :>> ", error);
-    }
-
-    console.log("files :>> ", formData);
-    file = files[0];
-    let results;
-    Papa.parse(file, {
-      delimiter: ",",
-
-      header: true,
-      complete: function (responses) {
-        results = responses.data;
-        file = results;
-        console.log("91:>>" + responses.data.length, responses);
-        setAsistentes(responses.data);
-      },
-    });
-  };
+  
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     console.log("file... :>> ", file);
+    console.log("file.name.. :>> ", file.name);
     const formData = new FormData();
     //{mode:'cors'}
-  //  fetch("https://doq6msba36.execute-api.us-east-1.amazonaws.com/PY/upload", {
-    
-  fetch("https://4geirpp1m2.execute-api.us-east-1.amazonaws.com/pd/subir", {
+  //  f, {
+    //  fetch("https://4geirpp1m2.execute-api.us-east-1.amazonaws.com/pd/subir", {
+      //fetch("https://doq6msba36.execute-api.us-east-1.amazonaws.com/PD/upload", {
+      //  fetch("https://h3c4knbj64.execute-api.us-east-1.amazonaws.com/pyy/subir", {
+       fetch(" https://doq6msba36.execute-api.us-east-1.amazonaws.com/PD/upload", {
+         
       method: "POST",
       body: file,
-   
-      // 👇 Set headers manually for single file upload
+      
       headers: {
-        "Content-Type": "application/json",
-        "content-length": `${file.size}`, // 👈 Headers need to be a string
-        "filename": "reacjs.pdf",
-        "bucket": "confaanexospy/MPC/beneficios/postulaciones",
-        "sistema": "MPC",
-        "access-control-allow-origin": "*"
+        "Content-Type": "multipart/form-data",
+        "sistema":"MPC",
+        "bucket":"confaanexospy/MPC/beneficios/postulaciones",
+        "filename":file.name
+        
       },
     })
       .then((res) => res.json())
       .then((data) => console.log(data))
       .catch((err) => console.error(err));
-
-    // formData.append("filename", name);
-    /*
-   const headers = {
-    "Content-Type": "application/json",
-    bucket:'confaanexospy/MPC/beneficios/postulaciones',
-    sistema:'MPC',
-    filename:'file.pdf'
-  };
-  const body={
-
-  }
-
-  const resp = await axios.post('https://doq6msba36.execute-api.us-east-1.amazonaws.com/PY/upload', formData,{  
-    headers: {
-      "Content-Type": "application/json",
-      bucket:'confaanexospy/MPC/beneficios/postulaciones',
-      sistema:'MPC'
-    
-    },
-      
-    });
-  */
   };
 
   return (
@@ -167,13 +59,13 @@ export default function CargarArchivo() {
           />
         </label>
 
-        <label>Cargar Archivo-21</label>
+        <label>Cargar Archivo-21--ok</label>
 
         <input type="file" onChange={(e) => setFile(e.target.files[0])} />
 
         <div style={{ height: 300, width: "80%" }}></div>
         <h1>{res}</h1>
-        <input type="submit" value="Cargar archivo 21" />
+        <input type="submit" value="Cargar archivo 22 ok" />
       </div>
     </form>
   );
